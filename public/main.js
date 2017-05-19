2c1b8d125227356a5bc2e453ee8cbba8dc88a310
NickSugar <nicksugar1@outlook.com>
2017-05-19 08:04:47 -0600

    finished large header

--- public/main.js
+++ public/main.js
@@ -1,1 +1,8 @@
 
+$(document).ready(function(){
+  console.log('hi');
+  $('.headerImg').parallax({
+    speed: 0.2,
+    imageSrc: 'https://i.ytimg.com/vi/3Uw2bBxTlU8/maxresdefault.jpg'
+  });
+});
